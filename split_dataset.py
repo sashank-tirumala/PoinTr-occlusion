dc3eb4f611faf1f596222c1a42656035f93aef22
sashank_tirumala <stsashank6@gmail.com>
2023-06-06 05:57:44 -0400

    Added code to split the dataset

---
+++ split_dataset.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import argparse
+import shutil
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--dataset-path', '-dp', type=Path, default='./', help='path to dataset')
+    args = argparser.parse_args()
+    dataset_path = args.dataset_path
+    data_path = dataset_path / 'data'
+    if data_path.exists():
+        shutil.rmtree(data_path)
+    files = [file for file in dataset_path.glob('*')]
+    train_percent = 0.8
+    train_num = int(len(files) * train_percent)
+    train_files = files[:train_num]
+    test_files = files[train_num:]
+    with open(dataset_path / 'train.txt', 'w') as f:
+        for file in train_files:
+            f.write(str(file) + '\n')
+    with open(dataset_path / 'val.txt', 'w') as f:
+        for file in test_files:
+            f.write(str(file) + '\n')
+    with open(dataset_path / 'test.txt', 'w') as f:
+        for file in test_files:
+            f.write(str(file) + '\n')    
+    pass
