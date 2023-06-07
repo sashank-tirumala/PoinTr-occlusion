5481af6c25c80211e23d841f8f96ebe1ddc58676
sashank_tirumala <stsashank6@gmail.com>
2023-06-07 12:17:09 -0400

    better config files, training works

--- split_dataset.py
+++ split_dataset.py
@@ -9,19 +9,22 @@ if __name__ == "__main__":
     dataset_path = args.dataset_path
     data_path = dataset_path / 'data'
     if data_path.exists():
-        shutil.rmtree(data_path)
-    files = [file for file in dataset_path.glob('*')]
-    train_percent = 0.8
-    train_num = int(len(files) * train_percent)
-    train_files = files[:train_num]
-    test_files = files[train_num:]
-    with open(dataset_path / 'train.txt', 'w') as f:
-        for file in train_files:
-            f.write(str(file) + '\n')
-    with open(dataset_path / 'val.txt', 'w') as f:
-        for file in test_files:
-            f.write(str(file) + '\n')
-    with open(dataset_path / 'test.txt', 'w') as f:
-        for file in test_files:
-            f.write(str(file) + '\n')    
-    pass
+        with open(data_path / 'train.txt', 'r') as f:
+            train_files = f.readlines()
+            breakpoint()
+    else:
+        files = [file for file in dataset_path.glob('*')]
+        train_percent = 0.8
+        train_num = int(len(files) * train_percent)
+        train_files = files[:train_num]
+        test_files = files[train_num:]
+        with open(dataset_path / 'train.txt', 'w') as f:
+            for file in train_files:
+                f.write(str(file) + '\n')
+        with open(dataset_path / 'val.txt', 'w') as f:
+            for file in test_files:
+                f.write(str(file) + '\n')
+        with open(dataset_path / 'test.txt', 'w') as f:
+            for file in test_files:
+                f.write(str(file) + '\n')    
+        pass
